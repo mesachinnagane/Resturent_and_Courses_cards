741610465c8d87ad2a75c5f37d6c1c4091b00ed7
Julius_Kingsley <hkadam013@protonmail.com>
2026-09-23 15:48:43 +0530

    added products

--- src/componants/Card2.jsx
+++ src/componants/Card2.jsx
@@ -5,20 +5,20 @@ const Card2 = () => {
     <>
       <div className="card" style={{ width: "18rem" }}>
         <img
-          src="https://pinchofyum.com/tachyon/Chicken-Caesar-Smash-Tacos.jpg?resize=840%2C1200"
+          src="https://diethood.com/wp-content/uploads/2014/06/bbq-chicken-pizza-3.jpg"
           className="card-img-top"
           alt="..."
           height={200}
           width={100}
         />
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
+          <h5 className="card-title">Pizza</h5>
           <p className="card-text">
             Some quick example text to build on the card title and make up the
             bulk of the card’s content.
           </p>
           <a href="#" className="btn btn-primary">
-            Go somewhere
+            Buy Now
           </a>
         </div>
       </div>
